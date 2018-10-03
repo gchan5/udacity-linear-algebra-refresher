ccc2787a6d7bc9b4afecc2b8770f01222a5235c6
Gilbert Chan <unusgilbert@gmail.com>
2018-10-02 19:28:12 -0700

    Add addition, subtraction, scalar multiplication methods

--- vector.py
+++ vector.py
@@ -19,3 +19,26 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def addition(self, v):
+        return [x+y for x, y in zip(self.coordinates, v.coordinates)]
+
+    def subtract(self, v):
+        return [x-y for x, y in zip(self.coordinates, v.coordinates)]
+        
+    def scalar_mult(self, multiplier):
+        return [i*multiplier for i in self.coordinates]
+
+    def magnitude(self):
+        return 0
+    
+    def direction(self):
+        return 0
+
+if __name__ == "__main__":
+    v = Vector([2, 2, 2])
+    v2 = Vector([3, 3, 3])
+
+    print(v.addition(v2))
+    print(v.subtract(v2))
+    print(v.scalar_mult(2))
